08e98169d65f19d50dcb098fc26fa7f51681094d
Дёмин Виктор <91539376+HubVik01@users.noreply.github.com>
2024-09-03 21:51:20 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,19 @@
+first = int(input())
+second = int(input())
+third = int(input())
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+    
+    
+# num = int(input('Введите число: '))
+# if num == 3:
+#     print(1)
+# elif num > 3:
+#     print(2)
+# elif num < 3:
+#     print(3)
+
